6a0878763fbfe544314951119b1945b3de782ded
Mohmad-Naser-alnakeshbandi <mhmdn1381@gmail.com>
2022-11-22 16:55:21 +0100

    upload the Fetcher

---
+++ Fetcher/index.js
@@ -0,0 +1,22 @@
+
+import axios from "axios"
+
+// Make a request for a user with a given ID
+
+axios.get('https://port19x.github.io/mensascrap/master.json')
+  .then(function (response) {
+    // handle success
+    console.table(response.data.body.erzbergerstraße.monday);
+    console.table(response.data.body.erzbergerstraße.tuesday);
+    console.table(response.data.body.erzbergerstraße.wednesday);
+    console.table(response.data.body.erzbergerstraße.thursday);
+    console.table(response.data.body.erzbergerstraße.friday);
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
